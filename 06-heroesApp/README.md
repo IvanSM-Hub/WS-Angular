802e57a264dd4cc24d4f788e08286cd2c4bb284f
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-04-11 13:00:34 +0200

    mod 06-heroesApp 'Añadir CRUD de heroes'

--- 06-heroesApp/README.md
+++ 06-heroesApp/README.md
@@ -4,6 +4,9 @@
 
 1. Clonar el proyecto.
 2. Ejecutar ```npm install```.
-3. Levantar backend ```npm run backend```.
-4. Ejecutar la app ```npm start``` o también ```ng serve -o```.
+3. Instalar Angular Material ```ng add @angular/material```.
+4. Instalar PrimeFlex ```npm install primeflex```.
+5. Instalar json-server V0.17.4 ```npm i --save-dev json-server@0.17.4```.
+6. Levantar backend ```npm run backend```.
+7. Ejecutar la app ```npm start``` o también ```ng serve -o```.
 
--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,15 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('',{ nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
     {id:'DC Comics', desc: 'DC - Comics'},
     {id:'Marvel Comics', desc: 'Marvel - Comics'},
   ]
+
+  constructor (
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    console.log({hero});
+    return hero;
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+
+    if ( this.heroForm.invalid) return;
+
+    if ( this.currentHero.id ) {
+      this.heroesService.updateHero( this.currentHero )
+        .subscribe( hero => {
+          // TODO: mostrar snackbar
+        } );
+      return;
+    }
+
+    this.heroesService.addHero( this.currentHero )
+      .subscribe( hero => {
+        // TODO mostrar snackbar, y navegar a /heroes/edit/ hero.id
+      } )
+
+  }
 
 }
