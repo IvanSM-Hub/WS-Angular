086f2019d64874d7b56135af015c760cf4ee564d
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-06-13 12:38:49 +0200

    mod 07-formApp validators

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
-    selector: 'auth-register-page',
     templateUrl: './register-page.component.html',
-    styles: ``,
+
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['',[ Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern) ]],
+    email: ['',[ Validators.required, Validators.pattern(this.validatorsService.emailPattern)  ], [ this.emailValidatorService ] ],
+    username: ['',[ Validators.required, this.validatorsService.cantBeStrider ]],
+    password: ['',[ Validators.required, Validators.minLength(6) ]],
+    password2: ['',[ Validators.required ]],
+
+  }, {
+    validators: [
+      this.validatorsService.isFieldEqualFildTwo('password','password2')
+    ]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService
+  ) { }
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField( this.myForm, field );
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+
+}
--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-    templateUrl: './basic-page.component.html',
-    styles: ``,
+  templateUrl: './basic-page.component.html',
+  styles: ``,
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -14,15 +14,56 @@ export class BasicPageComponent {
   // })
 
   public myForm: FormGroup = this.formBuilder.group({
-    name:[''],
-    price:[0],
-    inStorage:[0],
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
-  constructor ( private formBuilder: FormBuilder ) {}
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset({
+
+    })
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+
+    if (!this.myForm.controls[field]) return null;
+
+    console.log(field)
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+
+      console.log(key)
+
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido.';
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } carácteres.`;
+      }
+    }
+    return null;
+  }
 
   OnSave(): void {
+
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
     console.log(this.myForm.value)
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 
 }
