e3262a05c88f30d15555b7b215dad38a1ed3d222
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-06-20 13:38:40 +0200

    mod 08-selectores 'creación de una página de selectores dinámicos, alimentados por una api externa'

---
+++ 08-selectores/src/app/countries/services/CountriesService.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, combineLatest, map, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesServiceService {
+
+  private _baseUrl = 'https://restcountries.com/v3.1';
+
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.America,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania,
+  ];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public get regions(): Region[] {
+    return [ ...this._regions ];
+  }
+
+  getCountriesByRegion( region: Region ): Observable<SmallCountry[]> {
+
+    if( !region ) return of([]);
+
+    const url: string = `${ this._baseUrl }/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        map( countries => countries.map( country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        }))),
+      );
+  }
+
+  getCountryByAlphaCode( alphaCode: string ): Observable<SmallCountry> {
+    const url = `${this._baseUrl}/alpha/${ alphaCode }?fields=cca3,name,borders`;
+    return this.http.get<Country>( url )
+      .pipe(
+        map( country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }) )
+      )
+  }
+
+  getCountriesBordersByCodes( borders: string[] ): Observable<SmallCountry[]> {
+    if ( !borders || borders.length === 0 ) return of([]);
+    const countriesRequests: Observable<SmallCountry>[] = [];
+    borders.forEach( code => {
+      const request = this.getCountryByAlphaCode( code );
+      countriesRequests.push( request );
+    } );
+    return combineLatest( countriesRequests );
+  }
+
+}
