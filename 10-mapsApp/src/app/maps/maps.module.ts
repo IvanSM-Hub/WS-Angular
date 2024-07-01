0546ee3059038942f197bfcca5fc0dd0eaafb182
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-07-01 13:47:11 +0200

    add 10-mapsApp 'standalone components'

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -4,20 +4,21 @@ import { CommonModule } from '@angular/common';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 (mapboxgl as any).accessToken = 'pk.eyJ1IjoiaXZhbnNtLWRldiIsImEiOiJjbHh1aXZsdWMxemVoMnBzaDJxbXMzMGw0In0.-Mdrtu3A-T5wpvSO1chtvw';
 
-import { MapsRoutingModule } from './maps-routing.module';
-import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
-import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -26,7 +27,11 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
   ],
   imports: [
     CommonModule,
-    MapsRoutingModule
+    MapsRoutingModule,
+
+    // STANDALONE COMPONENTS
+    CounterAloneComponent,
+    SideMenuComponent,
   ]
 })
 export class MapsModule { }
