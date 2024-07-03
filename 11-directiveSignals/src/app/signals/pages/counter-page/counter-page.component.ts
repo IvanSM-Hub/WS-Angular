fca65b1bfa2beb7c428fbcaedda36ef3a3a00613
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-07-03 11:42:47 +0200

    add 11-directiveSignals 'add signals'

---
+++ 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styleUrl: './counter-page.component.css'
+})
+export class CounterPageComponent {
+
+  //Signal writable
+  public counter = signal(10);
+
+  // Solo lectura
+  public squareCounter = computed( () => this.counter() * this.counter() );
+
+  increaseBy( value: number ) {
+    this.counter.update( current => current + value );
+  }
+
+}
